59f1f06a512a77c2e1f1f88c1e60b5d8e59fe607
Andrew <artemdanilenko1997@gmail.com>
2025-02-07 21:55:13 +0200

    init project and first logic

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="https://static.vecteezy.com/system/resources/thumbnails/023/654/784/small_2x/golden-logo-template-free-png.png"
+        alt="React logo"
+      />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
